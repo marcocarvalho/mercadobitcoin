d0054d495c835224cf261ac60df677b35d010072
Marco Carvalho <marco@mcorp.io>
2015-12-18 21:15:50 +0000

    serialization on empty

--- spec/mercado_bitcoin/api/data/order_book_spec.rb
+++ spec/mercado_bitcoin/api/data/order_book_spec.rb
@@ -5,8 +5,18 @@ RSpec.describe MercadoBitcoin::Api::Data::OrderBook, type: :model do
 
   subject { described_class.new(options) }
 
-  it 'empty' do
-    expect(subject.asks).to eq([])
-    expect(subject.bids).to eq([])
+  context 'empty' do
+    it 'asks bids' do
+      expect(subject.asks).to eq([])
+      expect(subject.bids).to eq([])
+    end
+
+    it 'to_hash' do
+      expect(subject.to_hash).to eq({ asks: [], bids: [] })
+    end
+
+    it 'to_json' do
+      expect(subject.to_json).to eq({ asks: [], bids: [] }.to_json)
+    end
   end
 end
