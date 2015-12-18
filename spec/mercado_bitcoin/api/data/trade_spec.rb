a0bfb60e77c7b25b78f4c5b104035c63a603e27f
Marco Carvalho <marco@mcorp.io>
2015-12-18 12:04:20 +0000

    trade serialization

--- spec/mercado_bitcoin/api/data/trade_spec.rb
+++ spec/mercado_bitcoin/api/data/trade_spec.rb
@@ -15,6 +15,26 @@ RSpec.describe MercadoBitcoin::Api::Data::Trade, type: :model do
 
   subject { described_class.new(valid) }
 
+  it 'to_hash' do
+    expect(subject.to_hash).to eq({
+      price: 10.3,
+      amount: 1.0,
+      tid: 123123,
+      type: 'sell',
+      date: time
+    })
+  end
+
+  it 'to_json' do
+    expect(subject.to_json).to eq({
+      price: 10.3,
+      amount: 1.0,
+      tid: 123123,
+      type: 'sell',
+      date: time
+    }.to_json)    
+  end
+
   it '#price' do
     expect(subject.price).to eq(10.3)
   end
@@ -33,5 +53,12 @@ RSpec.describe MercadoBitcoin::Api::Data::Trade, type: :model do
 
   it '#date' do
     expect(subject.date).to eq(time)
+  end
+
+  it 'invalid tid' do
+    s = described_class.new(tid: 'abc')
+    expect(s.tid).to eq('abc')
+    s = described_class.new(tid: '123')
+    expect(s.tid).to eq(123)    
   end
 end
